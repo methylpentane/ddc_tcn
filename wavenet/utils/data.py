a0e53c14122749e6d2a9560394b6396648d8214a
methylpentane <v47r2bu9@gmail.com>
2020-11-26 20:57:15 +0900

    add ddc chart class feature for training of ddc dataset.

--- wavenet/utils/data.py
+++ wavenet/utils/data.py
@@ -9,6 +9,9 @@ import numpy as np
 import torch
 import torch.utils.data as data
 
+import pickle
+
+# general audio methods
 
 def load_audio(filename, sample_rate=16000, trim=True, trim_frame_length=2048):
     audio, _ = librosa.load(filename, sr=sample_rate, mono=True)
@@ -61,6 +64,12 @@ def mu_law_decode(output, quantization_channels=256):
 
     return waveform
 
+# end
+# Dataset class (original)
+    """
+    input: raw audio
+    output: raw audio
+    """
 
 class Dataset(data.Dataset):
     def __init__(self, data_dir, sample_rate=16000, in_channels=256, trim=False): #no trim for match timestep
@@ -149,4 +158,97 @@ class DataLoader(data.DataLoader):
             targets = audio[:, self.receptive_fields:, :]
             return self._variable(audio),\
                    self._variable(one_hot_decode(targets, 2))
+# end
+# Dataset class (ddc_onsetnet)
+    """
+    input: spectrum (one channel from original data)
+    output: onset prediction(0~1, 1channel)
+    """
 
+class Dataset_onset(data.Dataset):
+    def __init__(self, data_dir, selected_channel=0, in_channels=80, trim=False): #no trim for match timestep
+        super(Dataset, self).__init__()
+
+        self.in_channels = in_channels
+        self.trim = trim
+
+        self.root_path = data_dir
+        self.filenames = [x for x in sorted(os.listdir(data_dir))]
+
+    def __getitem__(self, index):
+        filepath = os.path.join(self.root_path, self.filenames[index])
+
+        with open(filepath, 'rb') as f:
+            file = pickle.load(f)
+            encoded_audio = file[1][selected_channel] # shape:(time, freq, channel)
+
+        return encoded_audio
+
+    def __len__(self):
+        return len(self.filenames)
+
+
+class DataLoader_onset(data.DataLoader):
+    def __init__(self, data_dir, receptive_fields,
+                 sample_size=0, sample_rate=16000, in_channels=256,
+                 batch_size=1, shuffle=True):
+        """
+        DataLoader for WaveNet
+        :param data_dir:
+        :param receptive_fields: integer. size(length) of receptive fields
+        :param sample_size: integer. number of timesteps to train at once.
+                            sample size has to be bigger than receptive fields.
+                            |-- receptive field --|---------------------|
+                            |------- samples -------------------|
+                            |---------------------|-- outputs --|
+        :param sample_rate: sound sampling rates
+        :param in_channels: number of input channels
+        :param batch_size:
+        :param shuffle:
+        """
+        dataset = Dataset(data_dir, sample_rate, in_channels)
+
+        super(DataLoader, self).__init__(dataset, batch_size, shuffle)
+
+        if sample_size <= receptive_fields:
+            raise Exception("sample_size has to be bigger than receptive_fields")
+
+        self.sample_size = sample_size
+        self.receptive_fields = receptive_fields
+
+        self.collate_fn = self._collate_fn
+
+    def calc_sample_size(self, audio):
+        return self.sample_size if len(audio[0]) >= self.sample_size\
+                                else len(audio[0])
+
+    @staticmethod
+    def _variable(data):
+        tensor = torch.from_numpy(data).float()
+
+        if torch.cuda.is_available():
+            return torch.autograd.Variable(tensor.cuda())
+        else:
+            return torch.autograd.Variable(tensor)
+
+    def _collate_fn(self, audio):
+        audio = np.pad(audio, [[0, 0], [self.receptive_fields, 0], [0, 0]], 'constant')
+
+        if self.sample_size:
+            sample_size = self.calc_sample_size(audio)
+
+            while sample_size > self.receptive_fields:
+                inputs = audio[:, :sample_size, :]
+                targets = audio[:, self.receptive_fields:sample_size, :]
+
+                yield self._variable(inputs),\
+                      self._variable(one_hot_decode(targets, 2))
+
+                audio = audio[:, sample_size-self.receptive_fields:, :]
+                sample_size = self.calc_sample_size(audio)
+        else:
+            targets = audio[:, self.receptive_fields:, :]
+            return self._variable(audio),\
+                   self._variable(one_hot_decode(targets, 2))
+
+# end
