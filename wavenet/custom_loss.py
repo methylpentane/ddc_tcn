736dde6121624cccc9e84452bb7844e035c63760
methylpentane <v47r2bu9@gmail.com>
2021-01-17 15:25:31 +0900

    weighted class for imbalance (BCE onset only)

--- wavenet/custom_loss.py
+++ wavenet/custom_loss.py
@@ -4,9 +4,8 @@ import warnings
 class LossForOneshot(torch.nn.Module):
     def __init__(self, out_channels):
         super().__init__()
-        self.loss_func_symbol = torch.nn.CrossEntropyLoss()
-        self.loss_func_onset = torch.nn.BCEWithLogitsLoss()
         self.out_channels = out_channels
+        self.pos_weight = 30.0
 
     def forward(self, outputs, targets):
         '''
@@ -20,15 +19,18 @@ class LossForOneshot(torch.nn.Module):
 
         with warnings.catch_warnings():
             warnings.simplefilter('ignore')
-            onset_index_list = onset_targets.nonzero()[:,1]
+            onset_index_list = onset_targets.nonzero()[:,1] # size [num_onsets]
+
+        onset_weight = torch.ones(onset_targets.shape[1]).cuda()
+        onset_weight[onset_index_list] = self.pos_weight
 
         if onset_index_list.shape[0] != 0:
             symbol_targets = symbol_targets[:, onset_index_list, :]
             symbol_outputs = symbol_outputs[:, onset_index_list, :]
-            symbol_loss = self.loss_func_symbol(symbol_outputs.view(-1, self.out_channels-1), symbol_targets.view(-1).long())
+            symbol_loss = torch.nn.functional.cross_entropy(symbol_outputs.view(-1, self.out_channels-1), symbol_targets.view(-1).long())
         else:
             symbol_loss = torch.zeros(1).cuda()
-        onset_loss = self.loss_func_onset(onset_outputs.view(-1), onset_targets.view(-1))
+        onset_loss = torch.nn.functional.binary_cross_entropy_with_logits(onset_outputs.view(-1), onset_targets.view(-1), weight=onset_weight)
         loss = onset_loss + symbol_loss
 
         return loss
